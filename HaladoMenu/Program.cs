39c9bcb7b8e8fea1469b1ff7ee07964d3203b7f4
Zsombor <szilagyizs@kkszki.hu>
2025-09-18 08:42:40 +0200

    A menupontok kidolgozása és kiírása

--- HaladoMenu/Program.cs
+++ HaladoMenu/Program.cs
@@ -10,6 +10,114 @@ namespace HaladoMenu
     {
         static void Main(string[] args)
         {
+            string nev = "";
+            int szulev = 0;
+            bool ffi = true;
+           int aktualisPont = 0;
+            do
+            {
+                bool select = false;
+                do
+                {
+                    ShowMenu(aktualisPont);
+                    switch (Console.ReadKey().Key)
+                    {
+                        case ConsoleKey.Enter:
+                            select = true;
+                            break;
+                        case ConsoleKey.UpArrow:
+                            if (aktualisPont > 0)
+                            {
+                                aktualisPont--;
+                            }
+                            break;
+                        case ConsoleKey.DownArrow:
+                            if (aktualisPont < 2)
+                            {
+                                aktualisPont++;
+                            }
+                            break;
+                        default:
+                            Console.Beep();
+                            break;
+                    }
+
+                }
+                while (!select);
+                switch (aktualisPont)
+                {
+                    case 0: //Adatbekérés
+                        Console.Clear();
+                        Console.WriteLine("Adja meg a nevét:");
+                        nev = Console.ReadLine();
+                        Console.WriteLine("Adja meg születési dátumát:");
+                        szulev = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Adja meg a nemét (f/n):");
+                        ffi = Console.ReadLine() == "f";
+                        Console.WriteLine("Adatait rögzítettük. Enterrel tovább...");
+                        break;
+                    case 1: //adatkiírás
+                        Console.WriteLine($"Neve {nev}");
+                        Console.WriteLine($"Születési éve: {szulev}");
+                        if (ffi)
+                        {
+                            Console.WriteLine("Neme: Férfi");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Neme: Nő");
+                        }
+                        Console.WriteLine("Enterrel tovább...");
+                        Console.ReadLine();
+                            break;
+                    case 2: //kilépés
+                        Console.Clear();
+                        Console.Write("Biztosan kilépsz? (i/n)");
+                        if (Console.ReadKey().Key != ConsoleKey.I)
+                        {
+                            aktualisPont = 0;
+                        }
+                        break;
+
+                        
+                }
+            }
+            while (aktualisPont != 2);
+        }
+        static void ShowMenu(int cPoint)
+        {
+            Console.Clear();
+            if (cPoint == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            Console.WriteLine("1 - Személyes adatok bekérése:");
+            if (cPoint == 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            Console.WriteLine("2 - Személyes adatok kiírása");
+            if (cPoint == 2)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            Console.WriteLine("3 - Kilépés");
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
